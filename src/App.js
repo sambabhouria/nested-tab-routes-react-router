43ebc4b492544013d3577f9dc8c42d35f3f8a71d
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-12-29 14:03:43 +0100

    MacBook-Pro:nested-tab-routes-react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import { Switch, Route, Link, } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const Profile = () => <div>You're on the Profile Tab</div>;
+const Comments = () => <div>You're on the Comments Tab</div>;
+const Contact = () => <div>You're on the Contact Tab</div>;
+
+
+class App extends Component {
+  render() {
+        const { path } = this.props.match;
+        // var isActive = this.context.router.route.location.pathname === this.props.to;
+        // var link = 'isActive' ? 'active' : ''
+
+        const { location } = this.props;
+        const homeClass = location.pathname === "/home" ? "active" : "";
+        const aboutClass = location.pathname.match("comments$") ? "active" : "";
+        const contactClass = location.pathname.match("contact$") ? "active" : "";
+    return (
+      <div>
+      <h1>Hey welcome to the tabs site!</h1>
+      <ol>
+       <li><Link to={`${path}`} className={homeClass}>Profile</Link></li>
+       <li> <Link to={`${path}/comments`} className={aboutClass}>Comments</Link></li>
+       <li> <Link to={`${path}/contact`} className={contactClass}>Contact</Link></li>
+      </ol>
+      <div className="tabs">
+        <Switch>
+          <Route path={`${path}`} exact component={Profile} />
+          <Route path={`${path}/comments`} component={Comments} />
+          <Route path={`${path}/contact`} component={Contact} />
+         </Switch>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
